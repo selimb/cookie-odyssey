cc8be5a85599d639baec500a1bb4560443ae4787
Selim Belhaouane <selim.belhaouane@gmail.com>
2021-07-14 09:40:18 -0400

    navbar

--- lib/cookie_odyssey_web/views/layout_view.ex
+++ lib/cookie_odyssey_web/views/layout_view.ex
@@ -4,4 +4,12 @@ defmodule CookieOdysseyWeb.LayoutView do
   def title() do
     "Cookie Odyssey"
   end
+
+  # Inpsired by https://elixirforum.com/t/nest-eex-templates/18462
+  def nav_item(assigns, do: children) do
+    render(
+      "nav_item.html",
+      put_in(assigns[:children], children)
+    )
+  end
 end
