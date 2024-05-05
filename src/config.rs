7f61efcccf1dd7fa714cd96fc981ea9879cafc94
Selim Belhaouane <selim.belhaouane@gmail.com>
2024-05-05 13:26:34 -0400

    setup deps and config

---
+++ src/config.rs
@@ -0,0 +1,25 @@
+use config::{Config, Environment};
+use serde::Deserialize;
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct AppConfig {
+    pub database_file: String,
+}
+
+impl AppConfig {
+    pub fn from_env() -> Result<AppConfig, String> {
+        let conf_builder = Config::builder()
+            .add_source(
+                Environment::with_prefix("APP")
+                    .try_parsing(true)
+                    .separator("_"),
+            )
+            .build()
+            .expect("Failed to setup config builder.");
+
+        let conf = conf_builder
+            .try_deserialize::<AppConfig>()
+            .map_err(|err| format!("Invalid config:\n{err}"));
+        conf
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
+use std::process::ExitCode;
+
+use crate::config::AppConfig;
+
+mod config;
+
+fn run() -> Result<(), String> {
+    dotenv::dotenv().map_err(|err| err.to_string())?;
+    init_tracing()?;
+    let conf = AppConfig::from_env()?;
+
     println!("Hello, world!");
+
+    Ok(())
+}
+
+fn init_tracing() -> Result<(), String> {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).map_err(|err| err.to_string())
+}
+
+fn main() -> ExitCode {
+    match run() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("{err}");
+            ExitCode::FAILURE
+        }
+    }
 }
