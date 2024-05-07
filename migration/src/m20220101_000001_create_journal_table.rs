49fd801bdcfefb9f136965226f4a69cdd813e496
Selim Belhaouane <selim.belhaouane@gmail.com>
2024-05-07 11:30:40 -0400

    setup migrations

--- migration/src/m20220101_000001_create_journal_table.rs
+++ migration/src/m20220101_000001_create_journal_table.rs
@@ -6,42 +6,39 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
-                    .if_not_exists()
+                    .table(Journal::Table)
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Journal::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Journal::Name).string().not_null())
+                    .col(ColumnDef::new(Journal::Slug).string().not_null())
+                    .col(ColumnDef::new(Journal::StartDate).date().not_null())
+                    .col(ColumnDef::new(Journal::EndDate).date().null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Journal::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum Journal {
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    Slug,
+    StartDate,
+    EndDate,
 }
